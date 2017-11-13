de578b605f16c4423965ea4b8324d97ef3f7c64d
Samuel Roux <samuel.roux@mail.mcgill.ca>
2017-11-13 15:41:53 -0500

    - Can now build on android - Modified buttons colours

--- Pass-The-Phone/platforms/ios/build/emulator/PassThePhone.app/app/shared/pointsListItems.ts
+++
@@ -1,5 +0,0 @@
-
-export class PointsListItems {
-
-    constructor (public name: string, public points: number, public extra:string){}
-  }
--- Pass-The-Phone/platforms/ios/build/emulator/PassThePhone.app/app/shared/triviaAnswer.ts
+++
@@ -1,5 +0,0 @@
-import {TriviaQuestion} from "./triviaQuestion";
-
-export class TriviaAnswer {
-    constructor(public question: TriviaQuestion, public  content: string){}
-}

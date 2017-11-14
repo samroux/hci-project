950f380c471fe223b11511c71b3a2f4c2fa4e1d5
Samuel Roux <samuel.roux@mail.mcgill.ca>
2017-11-13 19:36:31 -0500

    - "fix" android back button - Modified answer page to fix button not showing fully

--- Pass-The-Phone/app/main.ts
+++ Pass-The-Phone/app/main.ts
@@ -7,14 +7,14 @@ import { AppModule } from "./app.module";
 // application variable should already be included in the app.js file
 // Only do this on android
 var application = require('application');
-if (application.android) {
-    application.android.on(application.AndroidApplication.activityBackPressedEvent, backEvent);
-  }
+// if (application.android) {
+//     application.android.on(application.AndroidApplication.activityBackPressedEvent, backEvent);
+//   }
   
-  // This does the work on deciding if you want to go back
-  // arg.cancel = true will cancel navigating back
-  function backEvent(args) {
-    //stop the back button
-    args.cancel = true;
-  }
+//   // This does the work on deciding if you want to go back
+//   // arg.cancel = true will cancel navigating back
+//   function backEvent(args) {
+//     //stop the back button
+//     args.cancel = true;
+//   }
 platformNativeScriptDynamic().bootstrapModule(AppModule);
